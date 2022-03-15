876dd6be110ec1f6608673dead7bbba9aef17bd0
Kamran <muhammadkamran8586@gmail.com>
2022-03-15 18:52:06 +0500

    Adding loading bar

--- control/app.js
+++ control/app.js
@@ -1,5 +1,6 @@
 const menuItems = document.querySelector(".menu-items");
 const hamburgerLines = document.querySelector(".hamburger-lines");
+const loader = document.querySelector(".loader");
 const hamburgerLinesText = hamburgerLines.innerHTML;
 hamburgerLines.addEventListener("click", (e) => {
   menuItems.classList.toggle("show-nav");
@@ -12,3 +13,9 @@ document.body.addEventListener("click", function (e) {
   if (e.target != this) return;
   menuItems.classList.remove("show-nav");
 });
+setTimeout(() => {
+  loader.style.display = "none";
+  window.onscroll = () => {}
+}, 1000);
+
+window.onscroll = () => { window.scroll(0, 0); };
